a7e1896954bc6995c74ee413e2ddd89cd295a250
Optix <100604473+LeOptix@users.noreply.github.com>
2024-01-03 13:10:01 +0100

    Update Homepage.js

--- src/utils/Homepage.js
+++ src/utils/Homepage.js
@@ -1,3 +1,6 @@
+import 'React' from 'react';
+import 'Cases' from '../data/Cases.js";
+
 export function Homepage() {
     return (
         <main>
@@ -38,50 +41,7 @@ export function Homepage() {
             <section>
             <h2>Últimos casos</h2>
             <div class="cases" id="casos-container">
-                <div class="case-card">
-                    <h3>Nombre del Caso de Corrupción 1</h3>
-                    <p>Imputados: [Lista de imputados]</p>
-                    <p>Coste: [Cantidad de dinero involucrada]</p>
-                    <p>Contexto: [Descripción del contexto del caso]</p>
-                    <p>Proceso Judicial: [Información sobre el estado del proceso judicial]</p>
-                    <p>Documentos: [Enlaces a documentos relevantes]</p>
-                    <a href="" class="cta-button">
-                        Ver caso completo
-                    </a>
-                </div>
-            
-                <div class="case-card">
-                    <h3>Nombre del Caso de Corrupción 2</h3>
-                    <p>Imputados: [Lista de imputados]</p>
-                    <p>Coste: [Cantidad de dinero involucrada]</p>
-                    <p>Contexto: [Descripción del contexto del caso]</p>
-                    <p>Proceso Judicial: [Información sobre el estado del proceso judicial]</p>
-                    <p>Documentos: [Enlaces a documentos relevantes]</p>
-                    <a href="" class="cta-button">
-                        Ver caso completo
-                    </a>
-                </div>
-            <div class="case-card">
-                    <h3>Caso Gürtel</h3>
-                    <p>NIT: 1</p>
-                    <p>Contexto: Descripción del caso 1</p>
-                    <p>Coste: 0€ (euros)</p>
-                    <p>Imputados: José Luis, Villarejo, Fulano de España</p>
-                    <p>Judicial: false</p>
-                    <a href="undefined" class="cta-button">
-                        Ver trama completa
-                    </a>
-            </div><div class="case-card">
-                    <h3>Caso Kitchen</h3>
-                    <p>NIT: 2</p>
-                    <p>Contexto: Descripción del caso 2</p>
-                    <p>Coste: 152542123€ (euros)</p>
-                    <p>Imputados: José Luis, Villarejo, Fulano de España</p>
-                    <p>Judicial: true</p>
-                    <a href="undefined" class="cta-button">
-                        Ver trama completa
-                    </a>
-            </div></div>
+                <Cases></Cases>
             </section>
         </main>
     )
